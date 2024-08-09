5a78c0d268279a808cbf50661ed7d878d78705fa
Dikshant Sharma <sdikshant89@gmail.com>
2024-08-09 01:16:59 -0600

    Added models

---
+++ Model/LoginUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace LockBase_DMS_Service.Model
+{
+	public class LoginUser
+    {
+        public int Id {get; set;}
+
+        public string Username {get; set;}
+
+
+        public string Email {get; set;}
+
+
+        public string Password {get; set;}
+
+
+        public string Country_code {get; set;}
+
+
+        public string Phone_number {get; set;}
+
+
+        public DateTime CreateDate {get; set;}
+
+
+        public DateTime UpdateDate {get; set;}
+
+
+        public bool IsDeleted {get; set;}
+
+    }
+}
+
---
+++ Model/SecurityQuestion.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace LockBase_DMS_Service.Model
+{
+	public class SecurityQuestion
+	{
+		public int Id { get; set; }
+        public int Question { get; set; }
+    }
+}
